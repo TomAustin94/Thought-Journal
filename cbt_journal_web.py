2eed033d894dc15ba0e2af39fb2f30d05a67ffa2
Tom <thomas.michael.austin@gmail.com>
2024-08-16 18:37:30 +0100

    Updated to include a nice homepage

--- cbt_journal_web.py
+++ cbt_journal_web.py
@@ -2,7 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/form')
+def form():
+    # Assuming there's already a function to render the form
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 def journal():
     if request.method == 'POST':
         entry = {
